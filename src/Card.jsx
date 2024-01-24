90cf21e5faafb99654a1568e5d71a946257c1661
Taiwei Ko <152499488+macgeek39@users.noreply.github.com>
2024-01-24 10:12:36 -0600

    Finished learning card component

---
+++ src/Card.jsx
@@ -0,0 +1,13 @@
+
+function Card(){
+
+    return(
+        <div className="card">
+            <img className="card-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEPT94O5VDUkNbTzQKoiiyEQjfi8EZYpJdnrxQhco4PPvAPh35eQgWQ_KyVqXbW9SBQ0E&usqp=CAU" alt="profile picture" />
+            <h2 className="card-title">Sonlko</h2>
+            <p className="card-text">I do magic with money!</p>
+        </div>
+    );
+}
+
+export default Card
